ce28788957fd259cadcd551ccd8443614e185591
JW <34543031+jwcodee@users.noreply.github.com>
2025-12-09 00:42:34 -0800

    seq.cpp: fix test case

--- tests/seq.cpp
+++ tests/seq.cpp
@@ -9,9 +9,9 @@ main()
   std::string seq = "ACGTACACTGGACTGAGTCT";
   std::string rc = "AGACTCAGTCCAGTGTACGT";
   TEST_ASSERT_EQ(btllib::get_reverse_complement(seq), rc);
-  std::string ct = "ATGTATATTGGATGTAGTTT";
+  std::string ct = "ATGTATATTGGATTGAGTTT";
   TEST_ASSERT_EQ(btllib::get_bs_conversion(seq, "CT"), ct);
-  std::string ga = "ACATAAACTAAACTAAATCT";
+  std::string ga = "ACATACACTAAACTAAATCT";
   TEST_ASSERT_EQ(btllib::get_bs_conversion(seq, "GA"), ga);
   return 0;
 }
