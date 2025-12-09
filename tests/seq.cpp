908e084b3703572ffaf8c5335b383b94cbbdd187
JW <34543031+jwcodee@users.noreply.github.com>
2025-12-08 22:43:33 -0800

    seq.cpp: add bs conversion tests

--- tests/seq.cpp
+++ tests/seq.cpp
@@ -9,5 +9,9 @@ main()
   std::string seq = "ACGTACACTGGACTGAGTCT";
   std::string rc = "AGACTCAGTCCAGTGTACGT";
   TEST_ASSERT_EQ(btllib::get_reverse_complement(seq), rc);
+  std::string ct = "ATGTATATTGGATGTAGTTT";
+  TEST_ASSERT_EQ(btllib::get_bs_conversion(seq, "CT"), ct);
+  std::string ga = "ACATAAACTAAACTAAATCT";
+  TEST_ASSERT_EQ(btllib::get_bs_conversion(seq, "GA"), ga);
   return 0;
 }
